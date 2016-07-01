be1abee9aa9e9fb2cc673dae9378bded730e0433
geekduck <duck1218+github@gmail.com>
2016-07-01 21:02:30 +0900

    Initial commit.

---
+++ server.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var port = 8080;
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/admin/api/message', function(req, res) {
+    console.log("/admin/api/message");
+    io.sockets.emit("api message", {data:req.params.data});
+
+    res.json({
+        status: 200
+    });
+});
+
+io.on('connection', function(socket) {
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', "port:" + port + " user:" + socket.id + " msg:" + msg);
+    });
+});
+
+io.on('hogehoge', function() {
+  console.dir(arguments);
+});
+
+http.listen(port, function() {
+    console.log('listening on *:' + port);
+});
